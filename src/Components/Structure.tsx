82f6d1e8b9935cc749922fda3f632d4cf732d225
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 04:12:03 +0200

    add import

---
+++ src/Components/Structure.tsx
@@ -0,0 +1,6 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "./Header";
+import { Navigation } from "./Navigation";
+import { Footer } from "./Footer";
+import '../Styles/Structure.css';
+
