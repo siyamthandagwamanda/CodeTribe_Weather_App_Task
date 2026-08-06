bec541ec861edbeaecbdc87cb216cedc5b9aa65a
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 07:30:20 +0200

    update

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,7 +4,7 @@ import '../Styles/Footer.css';
 export const Footer = () => {
     return(
         <footer className="footer">
-            <p>&copy; 2026 W App @Mlab CodeTribe 26/27</p>
+            <p>&copy; 2026 Weather App @Mlab CodeTribe 26/27</p>
         </footer>
     )
 }
