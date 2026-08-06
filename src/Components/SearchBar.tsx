bc0ecafb8344c3b51b67d4537f24a5dcaacbcde3
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 04:54:59 +0200

    finished type properties

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -2,5 +2,7 @@ import React from "react";
 import '../Styles/SearchBar.css';
 
 type SearchBarProps = {
-    
+    city: string;
+    onCityChange: (value: string) => void;
+    onSearch: () => void;
 }
