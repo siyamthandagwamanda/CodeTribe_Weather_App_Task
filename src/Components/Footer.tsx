6d926530299d0e84f459a62c53049c5be7d90b45
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 04:03:41 +0200

    finish footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../Styles/Footer.css';
+
+export const Footer = () => {
+    return(
+        <footer className="footer">
+            <p>&copy; 2026 W App @Mlab CodeTribe 26/27</p>
+        </footer>
+    )
+}
