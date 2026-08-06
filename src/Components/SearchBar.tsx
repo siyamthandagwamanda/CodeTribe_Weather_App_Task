fb84326f124baf26047cd31061d912319d6825ae
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 05:06:28 +0200

    finish searchbar and form handler

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -5,4 +5,31 @@ type SearchBarProps = {
     city: string;
     onCityChange: (value: string) => void;
     onSearch: () => void;
-}
+};
+
+export const SearchBar = ({
+    city, 
+    onCityChange,
+    onSearch,
+
+}: SearchBarProps) => {
+    const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        onSearch();
+    };
+
+    return(
+        <form className="search-bar" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter a City"
+                   value={city} 
+                   onChange={(event) => onCityChange(event.target.value)} 
+            />
+            <button type="submit">
+                Search
+            </button>
+        </form>
+    )
+      
+};
+
+    
