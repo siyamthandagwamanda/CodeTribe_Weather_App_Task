d8bc3bd99b6e62c8807c5948c38d2f12d52a6661
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 03:57:15 +0200

    Updated NavLink components to leverage the built-in function syntax for the className attribute

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -2,3 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import '../Styles/Navigation.css';
 
+export const Navigation = () => {
+    return(
+        <nav className="navigation">
+            <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                Home
+            </NavLink>
+
+              <NavLink to="/favorites" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+                Favorites
+            </NavLink>
+        </nav>
+    )
+}
