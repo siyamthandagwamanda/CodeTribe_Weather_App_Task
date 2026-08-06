0d29e70a74369f96e6691a254bcaca83fc174952
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 03:09:05 +0200

    import BrowserRouter for app, Routes to render, Route to map

---
+++ src/Router/WeatherAppRouter.tsx
@@ -0,0 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { HomePage } from "../Pages/HomePage";
