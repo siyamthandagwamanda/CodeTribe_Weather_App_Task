4c4b97ed158778c59eeeb5f27be45af972d4f15c
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 05:30:00 +0200

    apply type safety, export to exists in anyplace

---
+++ src/Types/Weather.ts
@@ -0,0 +1,10 @@
+export type WeatherData{
+    city: string;
+    country: string;
+    temperature: number;
+    condition: string;
+    high: number;
+    low: number;
+    windSpped: number;
+    humidity: number;
+};
