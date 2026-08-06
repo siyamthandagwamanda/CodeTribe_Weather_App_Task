3a572acc50897fd727d016fa41b1af2d58fd9004
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-06 03:43:30 +0200

    finish header.tsx component for displaying purpose

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,5 +2,10 @@ import React from "react";
 import "../Styles/Header.css";
 
 export const Header = () => {
-    
+    return(
+        <header className='header'>
+            <h1>Weather App</h1>
+            <p>Know the weather before you go</p>
+        </header>
+    );
 }
