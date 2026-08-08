e173a7d2360775115673973c60dc15bd71e2a10c
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-09 01:16:32 +0200

    updates/fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import "../src/Styles/Global.css"
+
 import { WeatherAppRouter } from './Router/WeatherAppRouter';
-import "../src/App.css"
 
 export const App = () => {
   return <WeatherAppRouter />
