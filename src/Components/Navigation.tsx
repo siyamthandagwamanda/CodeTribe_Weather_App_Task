d1e4f1b16f4b4d348c5c8252e3ddbe5a3cdee2a0
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-08 19:40:09 +0200

    Links array

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -2,3 +2,8 @@ import { NavLink } from "react-router-dom";
 import "../Styles/Navigation.css";
 
 const navLinkClass = ({ isActive}: {isActive: boolean}) => isActive ? "nav-link active" : "nav-link";
+
+const links = [
+    { to: "/", label: "Home" },
+    { to: "MyLocations", label: "My Locations"},
+];
