c39116ad7a00f3fedaf31f9bc04b858de935b878
siyamthandagwmanda <gwamandasiyamthanda13@gmail.com>
2026-08-08 10:35:36 +0200

    Add WMO weather code to condition text mapping

--- src/Api/Weather.ts
+++ src/Api/Weather.ts
@@ -3,4 +3,21 @@ import type { WeatherData } from "../Types/Weather";
 const weatherConditions: Record<number, string> = {
   0: "Clear sky",
   1: "Mainly Clear",
-}
+  2: "Partly Cloudy",
+  3: "Overcast",
+  45: "Fog",
+  48: "Depositing Rime Fog",
+  51: "Light Drizzle",
+  53: "Moderate Drizzle",
+  55: "Dense Drizzle",
+  61: "Light Rain",
+  63: "Moderate Rain",
+  65: "Heavy Rain",
+  71: "Light Snow",
+  73: "Moderate Snow",
+  75: "Heavy Snow",
+  80: "Rain Showers",
+  81: "Heavy Rain Showers",
+  82: "Violent Rain Showers",
+  95: "Thunderstorm",
+};
