0741fe4030fe50553fb58f1bcd6722d8a94236da
gwamandasiyamthanda13@gmail.com <gwamandasiyamthanda13@gmail.com>
2026-08-10 12:23:56 +0200

    define settings types and create context

--- src/Context/WeatherSettings.tsx
+++ src/Context/WeatherSettings.tsx
@@ -1,2 +1,15 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import type { ReactNode } from "react";
+
+type TemperatureUnit = "C" | "F";
+type Theme = "light" | "dark";
+
+type WeatherSettingsContextType = {
+    temperatureUnite: TemperatureUnit;
+    theme: Theme;
+    toggleTemperatureUnit: () => void;
+    toggleTheme: () => void;
+};
+
+const WeatherSettings = createContext<WeatherSettingsContextType | undefined>(undefined);
+
