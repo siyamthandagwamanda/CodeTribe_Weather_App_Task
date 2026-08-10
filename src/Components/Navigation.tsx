7f77826bb0e3c4fb80aabd1a65a9250b70e0dc72
gwamandasiyamthanda13@gmail.com <gwamandasiyamthanda13@gmail.com>
2026-08-10 14:37:16 +0200

    ----

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import "../Styles/Navigation.css";
-import { useState } from "react";
-import { useWeatherSettings } from "../Context/WeatherSettings";
+// import { useState } from "react";
+// import { useWeatherSettings } from "../Context/WeatherSettings";
 
 const navLinkClass = ({ isActive}: {isActive: boolean}) => isActive ? "nav-link active" : "nav-link";
 
@@ -11,18 +11,25 @@ const links = [
 ];
 
 export const Navigation = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const { theme, temperatureUnit, toggleTheme, toggleTemperatureUnit } = useWeatherSettings();
+    // const [menuOpen, setMenuOpen] = useState(false);
+    // const { theme, temperatureUnit, toggleTheme, toggleTemperatureUnit } = useWeatherSettings();
     
-    const closeMenu = () => setMenuOpen(false);
+    // const closeMenu = () => setMenuOpen(false);
 
     return(
         <nav className="navigation">
-            {links.map(({ to, label }) => (
-                <NavLink key={to} to={to} className={navLinkClass}>
-                    {label}
-                </NavLink>
-            ))}
+            <div className="navigation-container"> 
+                <div className="navigation-links">
+                    {links.map(({ to, label }) => (
+                    <NavLink key={to} to={to} className={navLinkClass}>
+                        {label}
+                    </NavLink>
+                    ))}
+                </div>
+
+            </div>
+          
+          
         </nav>
    );
 }
