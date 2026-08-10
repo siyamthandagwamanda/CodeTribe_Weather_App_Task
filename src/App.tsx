0f8f1cac7712f7ffec21ccd67adc6cf0fc98fae7
gwamandasiyamthanda13@gmail.com <gwamandasiyamthanda13@gmail.com>
2026-08-10 13:34:18 +0200

    import settings provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "../src/App.css"
-
 import { WeatherAppRouter } from './Router/WeatherAppRouter';
+import { WeatherSettingsProvider } from "./Context/WeatherSettings";
 
 export const App = () => {
   return <WeatherAppRouter />
