5aa823c227ab131e50fb1ff7bdf84ffc8520e397
gwamandasiyamthanda13@gmail.com <gwamandasiyamthanda13@gmail.com>
2026-08-11 19:10:29 +0200

    importing component that will appear on my pages

--- src/Router/WeatherAppRouter.tsx
+++ src/Router/WeatherAppRouter.tsx
@@ -1,1 +1,4 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Structure } from "../Components/Structure";
+import { HomePage } from "../Pages/HomePage";
+import { MyLocationsPage } from "../Pages/MyLocationsPage";
