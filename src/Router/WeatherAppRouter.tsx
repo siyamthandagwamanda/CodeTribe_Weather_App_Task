d8b864287974c52cfa08876bb1dbd301418cf161
gwamandasiyamthanda13@gmail.com <gwamandasiyamthanda13@gmail.com>
2026-08-11 19:16:30 +0200

    create the pages in app

--- src/Router/WeatherAppRouter.tsx
+++ src/Router/WeatherAppRouter.tsx
@@ -2,3 +2,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Structure } from "../Components/Structure";
 import { HomePage } from "../Pages/HomePage";
 import { MyLocationsPage } from "../Pages/MyLocationsPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Structure />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: "my-locations",
+        element: <MyLocationsPage />
+      },
+    ]
+  }
+]);
