a8776b8abaee6d92f1ce72428c174571e9a8c93e
gwamandasiyamthanda13@gmail.com <gwamandasiyamthanda13@gmail.com>
2026-08-27 19:11:23 +0200

    feat(geolocation): wrap browser geolocation API in a Promise

---
+++ src/Utils/Geolocation.ts
@@ -0,0 +1,15 @@
+
+export function getCurrentPosition(): Promise<GeolocationPosition> {
+  return new Promise((resolve, reject) => {
+    if (!("geolocation" in navigator)) {
+      reject(new Error("Geolocation is not supported by this browser."));
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(resolve, reject, {
+      enableHighAccuracy: true,
+      timeout: 10000,
+      maximumAge: 5 * 60 * 1000,
+    });
+  });
+}
